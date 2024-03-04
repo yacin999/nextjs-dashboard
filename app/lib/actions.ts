f2e4d9845cf49e9e639f35dc84f3c09730f02f83
yacine <yacinomar999@gmail.com>
2024-03-04 09:44:23 +0100

    add login and logout

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -112,7 +114,7 @@ export async function updateInvoice(id : string , prevState : State, formData : 
 
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
@@ -122,5 +124,28 @@ export async function deleteInvoice(id: string) {
     return {
       message : 'Database Error: Failed to Delete Invoice.' 
     }
+  }
+}
+
+
+
+
+// authenticate users :
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
--- auth.ts
+++ auth.ts
@@ -20,20 +20,26 @@ async function getUser(email: string): Promise<User | undefined> {
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers : [
+  providers: [
     Credentials({
-        // async authorize(credentials) {
-        //   const parsedCredentials = z
-        //     .object({ email: z.string().email(), password: z.string().min(6) })
-        //     .safeParse(credentials);
+      async authorize(credentials) {
+        const parsedCredentials = z
+            .object({ email: z.string().email(), password: z.string().min(6) })
+            .safeParse(credentials);
 
-        //     if (parsedCredentials.success) {
-        //         const { email, password } = parsedCredentials.data;
-        //         const user = await getUser(email);
-        //         if (!user) return null;
-        //     }
-        //     const passwordsMatch = await bcrypt.compare(password, user.password);
-        // },
+ 
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+  
+        console.log('Invalid credentials');
+        return null;
+      },
     }),
-  ]
+  ],
 });
